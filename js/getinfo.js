0d76d3fad42e1c92311c978f005379a83d5c55f4
Wish <100239673+wish13yt@users.noreply.github.com>
2025-02-22 15:58:54 -0600

    add more stuff

--- js/getinfo.js
+++ js/getinfo.js
@@ -1,5 +1,8 @@
 function getInfo() {
     useragent.innerText = "Useragent: " + navigator.userAgent
+    cookie.innerText = "Can websites set cookies: " + navigator.cookieEnabled
+    ram.innerText = "Amount of RAM: " + navigator.deviceMemory + " (approximate, heres why: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory)"
+    posture.innerText = "Device posture: " + navigator.devicePosture.type + " (experimental feature, see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/devicePosture)"
     console.log(navigator)
 }
 function getIP() {
