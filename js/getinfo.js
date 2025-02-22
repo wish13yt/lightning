db0eb6cd523c4f8a256a7e5d7684d679c7f4c8bc
Wish <100239673+wish13yt@users.noreply.github.com>
2025-02-22 16:22:50 -0600

    fix firefox glitching out

--- js/getinfo.js
+++ js/getinfo.js
@@ -1,14 +1,14 @@
 function getInfo() {
     useragent.innerText = "Useragent: " + navigator.userAgent
     cookie.innerText = "Can websites set cookies: " + navigator.cookieEnabled
-    if ($.browser.mozilla) {
+    if (navigator.userAgent.indexOf("Firefox") > 0) {
         ram.innerText = "Sorry! Firefox does not support detecting RAM."
         posture.innerText = "Sorry! Firefox does not support detecting device posture."
     } else {
         ram.innerText = "Amount of RAM: " + navigator.deviceMemory + " (approximate, heres why: https://developer.mozilla.org/en-US/docs/Web/API/Navigator/deviceMemory)"
         posture.innerText = "Device posture: " + navigator.devicePosture.type + " (experimental feature, see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/devicePosture)"
     }
-    points.innerText = "Amount of touch points " + navigator.maxTouchPoints + " (pretty much just how many fingers you can use on your device at once)"
+    points.innerText = "Amount of touch points: " + navigator.maxTouchPoints + " (pretty much just how many fingers you can use on your device at once)"
     console.log(navigator)
 }
 function getIP() {
